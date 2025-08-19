2973ce893ad61c803e299a8db503f7ee13c22714
Ruby <ruben@kindworks.ai>
2025-08-19 18:08:38 +0300

    multi-filter partitioned search working

--- examples/cpp/filter.cpp
+++ examples/cpp/filter.cpp
@@ -10,9 +10,17 @@
 
 typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> RowMatrix;
 std::vector<std::unordered_set<int>> attributes;
-const int n_attributes = 10;
+const int n_attributes = 30;
 // const int n_input_vecs = 50000; //999994
-std::vector<std::string> attribute_strings = {"red", "green", "blue", "yellow", "orange", "purple", "black", "white", "pink", "brown"};
+//std::vector<std::string> attribute_strings = {"one", "green", "blue", "yellow", "orange", "purple", "black", "white", "pink", "brown"};
+std::vector<std::string> attribute_strings = {
+        "one", "two", "three", "four", "five",
+        "six", "seven", "eight", "nine", "ten",
+        "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+        "sixteen", "seventeen", "eighteen", "nineteen", "twenty",
+        "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five",
+        "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", "thirty"
+    };
 std::vector<int> attribute_idxs(attribute_strings.size());
 std::random_device rd; // obtain a random number from hardware
 std::mt19937 gen(42); // seed the generator
@@ -58,11 +66,9 @@ RowMatrix* load_vectors(int n_input_vecs=999994) {
     std::unordered_set<int> point_attributes(n_attributes_for_point);
     for (int k = 0; k < n_attributes_for_point; ++k) {
       int selected_attr_idx = attribute_selector_distr(gen);
-      // std::string random_attribute = attribute_strings[selected_attr_idx];
       point_attributes.insert(selected_attr_idx);
     }
     
-    // std::cout << "random attribute: " << random_attribute << std::endl;
     attributes.push_back(point_attributes);
     while (iss >> value) {
       (*ret_ptr)(i, j) = value;
@@ -70,7 +76,7 @@ RowMatrix* load_vectors(int n_input_vecs=999994) {
     }
     ++i;
   }
-
+  std::cout << "Loading data complete" << std::endl;
   return ret_ptr;//().topRows(100000);
 }
 
@@ -84,7 +90,7 @@ extern "C" {
     Q_ptr = X;
     // RowMatrix Q = X.topRows(1000);
     // Q_ptr =  new RowMatrix(X->topRows(100000));
-    for (int i = 0; i <= attribute_idxs.size(); ++i) {
+    for (int i = 0; i < attribute_strings.size(); ++i) {
       attribute_idxs.push_back(i);
     }
 
@@ -109,51 +115,16 @@ float filter(int q_idx, bool exact_search, int k,  int clusters_to_search, int p
   }
   Lorann::Lorann<Lorann::SQ4Quantizer> index = *index_ptr;
   RowMatrix Q = (*Q_ptr).topRows(1000);
-  // std::cout << "mark filter_attribute: " << filter_attribute << std::endl;
   auto it = std::find(attribute_strings.begin(), attribute_strings.end(), filter_attribute);
   int filter_idx = it - attribute_strings.begin();
   std::unordered_set<int> filter_attributes = {filter_idx};
-  // std::cout << "q_idx: " << q_idx << std::endl;
   Eigen::VectorXi exact_indices(k);
   Eigen::VectorXi approx_indices(k);
-  // std::cout << "Q.row(q_idx).data() " << Q.row(q_idx).data()[0] << std::endl;
-
-  // std::cout << "mark 3" << std::endl;
-  // std::cout << "----" << std::endl;
-  // std::cout << "Q rows: " << Q.rows() << std::endl;
-  // std::cout << "Q cols: " << Q.cols() << std::endl;
-
-  // std::cout << "Querying the index using exact search..." << std::endl;
-  // std::cout << "running exact search with " << filter_approach << " strategy and k: " << k << std::endl;
   index.exact_search((*Q_ptr).row(q_idx).data(), k, exact_indices.data(), filter_attributes, filter_approach);
-  
-  
-  // std::cout << std::endl << "Querying the index using approximate search..." << std::endl;
-    // std::cout << "running approximate search with " << filter_approach << " strategy and k: " << k << std::endl;
   bool verbose = false;
   index.search((*Q_ptr).row(q_idx).data(), k, clusters_to_search, points_to_rerank, approx_indices.data(), filter_attributes, filter_approach, nullptr, verbose);
-  // std::cout << exact_indices.transpose() << std::endl;
-  // // std::cout << "approx search results:" << std::endl;
-  // std::cout << approx_indices.transpose() << std::endl;
-  // for (const auto& idx : approx_indices) {
-  //   std::cout << attributes[idx] << " ";
-  // }
-  // for (const auto& idx : exact_indices) {
-  //   std::cout << attributes[idx] << " ";
-  // }
   std::vector<int> res_union = findUnion(exact_indices, approx_indices);
   float recall = res_union.size()/float(k);
-  // std::cout << "Recall: " << recall << std::endl;
-  
-  // if (exact_search) {
-    
-  // } else {
-    
-  // }
-  // std::cout << std::endl << "Saving the index to disk..." << std::endl;
-  // std::ofstream output_file("index.bin", std::ios::binary);
-  // cereal::BinaryOutputArchive output_archive(output_file);
-  // output_archive(index);
   return recall;
 }
 }
@@ -184,14 +155,31 @@ extern "C" {
 }
 
 extern "C" {
-  float fast_filter_wrapper_profiled(int* idxs, int n_idxs, int k, int clusters_to_search, int points_to_rerank, const char* filter_attribute, const char* filter_approach, const char* exact_search_approach) {
+  float fast_filter_wrapper_profiled(
+    int* idxs,
+    int n_idxs,
+    int k,
+    int clusters_to_search,
+    int points_to_rerank,
+    const char** string_filter_attributes,
+    int n_filter_attributes,
+    const char* filter_approach,
+    const char* exact_search_approach) {
     Lorann::Lorann<Lorann::SQ4Quantizer> index = *index_ptr;
-    auto it = std::find(attribute_strings.begin(), attribute_strings.end(), filter_attribute);
-    int filter_idx = it - attribute_strings.begin();
-    std::unordered_set<int> filter_attributes = {filter_idx};
+    std::unordered_set<int> filter_attributes;
+    std::cout << "n_filter_attributes: " << n_filter_attributes << std::endl;
+    for (int i = 0; i<n_filter_attributes; ++i) {
+      std::cout << string_filter_attributes[i] << std::endl;
+      auto it = std::find(attribute_strings.begin(), attribute_strings.end(), string_filter_attributes[i]);
+      int filter_idx = it - attribute_strings.begin();
+      filter_attributes.insert(filter_idx);
+    }
     std::vector<float> recall_vec(n_idxs);
     std::chrono::microseconds total_exact_duration = (std::chrono::microseconds) 0;
     std::chrono::microseconds total_approx_duration = (std::chrono::microseconds) 0;
+    std::cout << "Beginning querying..." << std::endl;
+    std::vector<Eigen::VectorXi> all_exact_indices(n_idxs);
+    std::vector<Eigen::VectorXi> all_approx_indices(n_idxs);
     for ( int i = 0; i < n_idxs; i++) {
       Eigen::VectorXi exact_indices(k);
       auto start_exact = std::chrono::high_resolution_clock::now();
@@ -199,15 +187,49 @@ extern "C" {
       auto stop_exact = std::chrono::high_resolution_clock::now();
       auto duration_exact = std::chrono::duration_cast<std::chrono::microseconds>(stop_exact - start_exact);
       total_exact_duration = total_exact_duration + duration_exact;
+      all_exact_indices.push_back(exact_indices);
       Eigen::VectorXi approx_indices(k);
       auto start_approx = std::chrono::high_resolution_clock::now();
       index.search((*Q_ptr).row(idxs[i]).data(), k, clusters_to_search, points_to_rerank, approx_indices.data(), filter_attributes, filter_approach, nullptr, false);
       auto stop_approx = std::chrono::high_resolution_clock::now();
       auto duration_approx = std::chrono::duration_cast<std::chrono::microseconds>(stop_approx - start_approx);
       total_approx_duration = total_approx_duration + duration_approx;
+      all_approx_indices.push_back(approx_indices);
       std::vector<int> res_union = findUnion(exact_indices, approx_indices);
       recall_vec[i] = res_union.size()/float(k);
     }
+    int exact_indices_true_matches = 0;
+    for (const auto& exact_indices: all_exact_indices) {
+      for (const auto& idx: exact_indices) {
+        std::unordered_set<int> idx_attributes = attributes[idx];
+        bool all_match = true;
+        for (int filter_idx: filter_attributes) {
+          if (!idx_attributes.count(filter_idx)) {
+            all_match = false;
+            
+          }  
+        }
+        if (all_match) exact_indices_true_matches++;
+        // std::unordered_set<int>::const_iterator it = idx_attributes.find(filter_idx);
+        // std::cout << attribute_strings[*it] << " ";
+      }
+    }
+    std::cout << "Exact indices match rate: " << ((double) exact_indices_true_matches) / (n_idxs*k) << std::endl;
+    int approx_indices_true_matches = 0;
+    for (const auto& approx_indices: all_approx_indices) {
+      for (const auto& idx: approx_indices) {
+        std::unordered_set<int> idx_attributes = attributes[idx];
+        bool all_match = true;
+        for (int filter_idx: filter_attributes) {
+          if (!idx_attributes.count(filter_idx)) {
+            all_match = false;
+            
+          }  
+        }
+        if (all_match) approx_indices_true_matches++;
+      }
+    }
+    std::cout << "Approximate indices match rate: " << ((double) approx_indices_true_matches) / (n_idxs*k) << std::endl;
     std::chrono::microseconds avg_exact_duration = total_exact_duration / n_idxs;
     std::chrono::microseconds avg_approx_duration = total_approx_duration / n_idxs;
     std::cout << "Average exact query duration: " << avg_exact_duration.count() << " microseconds" << std::endl;
